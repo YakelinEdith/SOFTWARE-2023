b7d0b9559cd4d6fc676aba9544dc603f90292425
YakelinEdith <yakelinedith10@gmail.com>
2023-05-01 15:32:30 -0600

    segundo commit

---
+++ UberPOO/Javascript/index.js
@@ -0,0 +1,7 @@
+var car = new Car("ASDF3652", new Account("Andrea Sanchez", "ASD123365214"))
+car.passenger= 4;
+car.printDataCar();
+
+var uberX = new UberX("DFGG5942", new Account("Marcos Caceres", "DFG26589"))
+uberX.passenger = 6;
+uberX.printDataCar();
